683d6e33dd6b335a01d7d6d0fd43723f98bcd662
Anwesh43 <anweshthecool0@gmail.com>
2018-09-17 23:20:11 +0530

    adding LineToTriangle class

--- LineToTriangleStage.ts
+++ LineToTriangleStage.ts
@@ -132,3 +132,25 @@ class LTSNode {
         return this
     }
 }
+
+class   LineToTriangle {
+    curr : LTSNode = new LTSNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
